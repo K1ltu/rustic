ec804c1ea26f0b3d9f11e52064d16c954f283084
k1ltu <m.petry0987@gmail.com>
2018-07-08 05:07:16 -0400

    cleaned up / optimized code tagged certain stuff with ReSharper tags and added reasons why inspection was disabled on certain elements

--- rustic/Form1.cs
+++ rustic/Form1.cs
@@ -8,58 +8,59 @@ namespace rustic
 {
     public partial class Form1 : Form
     {
-        String rustExe = String.Empty;
-        String customMap = String.Empty;
-        private Process proc1;
+        private string _rustExe = string.Empty;
+        private Process _proc1;
+
         public Form1()
         {
             InitializeComponent();
-            this.ServerWorldSizeField.SelectedIndex = 0;
-            this.ServerLevelType.SelectedIndex = 0;
-            this.ServerRconWebField.SelectedIndex = 0;
-            this.ServerMaxPlayers.SelectedIndex = 3;
-            this.buttonLaunch.Enabled = false;
+            ServerWorldSizeField.SelectedIndex = 0;
+            ServerLevelType.SelectedIndex = 0;
+            ServerRconWebField.SelectedIndex = 0;
+            ServerMaxPlayers.SelectedIndex = 3;
+            buttonLaunch.Enabled = false;
         }
 
         private void buttonExecuatable_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
-            openFileDialog1.RestoreDirectory = true;
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            var openFileDialog1 = new OpenFileDialog
             {
-                try
-                {
-                    rustExe = openFileDialog1.FileName;
-                    this.labelExecuatable.Text = rustExe;
-                    this.buttonLaunch.Enabled = true;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
-                }
+                InitialDirectory = "c:\\",
+                Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                _rustExe = openFileDialog1.FileName;
+                labelExecuatable.Text = _rustExe;
+                buttonLaunch.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
             }
         }
 
         private void buttonMap_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "map files (*.map)|*.map|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
-            openFileDialog1.RestoreDirectory = true;
+            var openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "map files (*.map)|*.map|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    customMap = "file:\\\\\\" + $"{openFileDialog1.FileName}";
-                    this.ServerMapUrl.Text = customMap;
+                    this.ServerMapUrl.Text = $"file:\\\\\\{openFileDialog1.FileName}";
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    MessageBox.Show($"Error: Could not read file from disk. Original error: {ex.Message}");
                 }
             }
         }
@@ -68,103 +69,123 @@ namespace rustic
         {
             // All the magic happens here
             var launchArgs = new StringBuilder();
-            if (this.ServerBatchMode.Checked)
+            if (ServerBatchMode.Checked)
             {
                 launchArgs.Append("-batchmode ");
             }
+
             try
             {
-                Convert.ToInt32(this.ServerPort.Text);
+                // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                // checks if the input is a valid interger
+                Convert.ToInt32(ServerPort.Text);
                 launchArgs.Append($"+server.port {ServerPort.Text} ");
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 MessageBox.Show("Error: Malformed or Blank server port");
                 return;
             }
-            if (this.ServerLevelType.SelectedIndex == 0)
+
+            if (ServerLevelType.SelectedIndex == 0)
             {
-                if (this.ServerMapUrl.Text.Equals(String.Empty))
+                if (ServerMapUrl.Text.Equals(String.Empty))
                 {
                     MessageBox.Show("There is no level url set");
                     return;
                 }
-                launchArgs.Append($"-levelurl \"{this.ServerMapUrl.Text}\" ");
+
+                launchArgs.Append($"-levelurl \"{ServerMapUrl.Text}\" ");
             }
             else
             {
                 try
                 {
-                    Convert.ToInt32(this.ServerSeedField.Text);
-                    launchArgs.Append($"+server.level \"Procedural Map\" +server.seed {this.ServerSeedField.Text} +server.worldsize {this.ServerWorldSizeField.Text} ");
+                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                    Convert.ToInt32(ServerSeedField.Text);
+                    launchArgs.Append(
+                        $"+server.level \"Procedural Map\" +server.seed {ServerSeedField.Text} +server.worldsize {ServerWorldSizeField.Text} ");
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     MessageBox.Show("Error: Malformed or Blank world seed");
                     return;
                 }
-                
             }
-            launchArgs.Append($"+server.maxplayers {this.ServerMaxPlayers.Text} ");
-            if (this.ServerHostname.Checked)
+
+            launchArgs.Append($"+server.maxplayers {ServerMaxPlayers.Text} ");
+            if (ServerHostname.Checked)
             {
-                launchArgs.Append($"+server.hostname \"{this.ServerHostnameField.Text}\" ");
+                launchArgs.Append($"+server.hostname \"{ServerHostnameField.Text}\" ");
             }
-            if (this.ServerDesc.Checked)
+
+            if (ServerDesc.Checked)
             {
-                launchArgs.Append($"+server.description \"{this.ServerDescField.Text}\" ");
+                launchArgs.Append($"+server.description \"{ServerDescField.Text}\" ");
             }
-            if (this.ServerUrl.Checked)
+
+            if (ServerUrl.Checked)
             {
-                launchArgs.Append($"+server.url \"{this.ServerUrlField.Text}\" ");
+                launchArgs.Append($"+server.url \"{ServerUrlField.Text}\" ");
             }
-            if (this.ServerImage.Checked)
+
+            if (ServerImage.Checked)
             {
-                launchArgs.Append($"+server.headerimage \"{this.ServerImageField.Text}\" ");
+                launchArgs.Append($"+server.headerimage \"{ServerImageField.Text}\" ");
             }
-            if (this.ServerIdentity.Checked)
+
+            if (ServerIdentity.Checked)
             {
-                launchArgs.Append($"+server.identity \"{this.ServerIdentityField.Text}\" ");
+                launchArgs.Append($"+server.identity \"{ServerIdentityField.Text}\" ");
             }
-            if (this.ServerRconPort.Checked)
+
+            if (ServerRconPort.Checked)
             {
                 try
                 {
-                    Convert.ToInt32(this.ServerRconPortField.Text);
-                    launchArgs.Append($"+rcon.port {this.ServerRconPortField.Text} ");
+                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                    // checks if the input is a valid interger
+                    Convert.ToInt32(ServerRconPortField.Text);
+                    launchArgs.Append($"+rcon.port {ServerRconPortField.Text} ");
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     MessageBox.Show("Error: Malformed or Blank RCON port");
                     return;
                 }
             }
-            if (this.ServerRconPassword.Checked)
+
+            if (ServerRconPassword.Checked)
             {
-                launchArgs.Append($"+rcon.password {this.ServerRconPasswordField.Text} ");
+                launchArgs.Append($"+rcon.password {ServerRconPasswordField.Text} ");
             }
-            if (this.ServerRconWeb.Checked)
+
+            if (ServerRconWeb.Checked)
             {
-                launchArgs.Append($"+rcon.web {this.ServerRconWebField.Text} ");
+                launchArgs.Append($"+rcon.web {ServerRconWebField.Text} ");
             }
-            if (this.Args.Checked)
+
+            if (Args.Checked)
             {
                 MessageBox.Show(launchArgs.ToString());
                 Clipboard.SetText(launchArgs.ToString());
             }
-            proc1 = new Process
+
+            _proc1 = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = rustExe,
-                    WorkingDirectory = Path.GetDirectoryName(rustExe),
-                    Arguments = $"{launchArgs.ToString()}",
+                    FileName = _rustExe,
+                    // ReSharper disable once AssignNullToNotNullAttribute
+                    // launch button is disabled until _rustExe is set
+                    WorkingDirectory = Path.GetDirectoryName(_rustExe),
+                    Arguments = $"{launchArgs}",
                     UseShellExecute = false,
                     RedirectStandardOutput = false,
                     CreateNoWindow = false
                 }
             };
-            proc1.Start();
+            _proc1.Start();
         }
 
         private void buttonExit_Click(object sender, EventArgs e)
